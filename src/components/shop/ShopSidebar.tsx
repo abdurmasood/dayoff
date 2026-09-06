f9f8c219b8f3875eaf7b9052edd0598084b9878d
Abdur Rafey Masood <abdurrafeym@gmail.com>
2026-09-06 23:02:14 +0100

    Align shop phone chrome so department pages share one ticker, header, and nav.

--- src/components/shop/ShopSidebar.tsx
+++ src/components/shop/ShopSidebar.tsx
@@ -1,40 +1,41 @@
 import Link from 'next/link'
 import { DepartmentNav } from '@/components/DepartmentNav'
 import { INITIAL_MATERIALS } from './filters'
+import { ShopCartBadge } from './ShopCartBadge'
 import { ShopFilterRow } from './ShopFilterRow'
 
 export function ShopSidebar() {
   return (
     <div className="sidebar">
       <div>
-        <Link href="/" className="sidebar-header">
-          <span>Day Off</span>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <line x1="12" y1="5" x2="12" y2="19" />
-            <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
-        </Link>
+        <div className="sidebar-chrome">
+          <Link href="/" className="sidebar-header">
+            <span>Day Off</span>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </Link>
+          <ShopCartBadge />
+        </div>
 
-        <details className="shop-menu">
-          <summary>MENU</summary>
-          <DepartmentNav activeId="tops" className="nav-list" />
+        <DepartmentNav activeId="tops" className="nav-list" />
 
-          <div className="filters">
-            <div className="filter-group">
-              <div className="filter-title">MATERIAL</div>
-              {INITIAL_MATERIALS.map((item) => (
-                <ShopFilterRow key={item.id} item={item} />
-              ))}
-            </div>
+        <div className="filters">
+          <div className="filter-group">
+            <div className="filter-title">MATERIAL</div>
+            {INITIAL_MATERIALS.map((item) => (
+              <ShopFilterRow key={item.id} item={item} />
+            ))}
           </div>
-        </details>
+        </div>
       </div>
 
       <div className="sidebar-footer">
